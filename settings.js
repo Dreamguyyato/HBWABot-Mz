ac1892a0f2a4b3194d01db4300a1b5e4d1aa328e
Dreamguyyato <dreamguykiyo@gmail.com>
2024-07-08 22:34:57 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const chalk = require("chalk")
 const fs = require("fs")
 
 //aumto presence update
-global.AntiCallBot = true // false a i dah chuan, rawn call-tu che automatic a block a ni lovang
+global.AntiCallBot = false // false a i dah chuan, rawn call-tu che automatic a block a ni lovang
 global.GroupWcMsg = false // false a i dah chuan welcome message i off ang
 global.autoTyping = false // true a i dah chuan i type la a lang ang
 global.autoRecord = false // true a i dah chuan Voice record angin i lang ang
@@ -17,21 +17,21 @@ global.bot_language = 'lus' // I language code dah tur code hman i hriat loh chu
 
 //info
 global.owner = ['916009259402'] // I phone number in thlak rawh
-global.ownernumber = '916009259402' // I phone number in thlak rawh
-global.ownername = "🦄드림 가이 yato" // I hming dah tel bawk la
+global.ownernumber = '918798751883' // I phone number in thlak rawh
+global.ownername = "dengi" // I hming dah tel bawk la
 global.location = "India, Mizoram, Aizawl" // I awmna hmun daha
-global.botname = "Dg Yato" // I bot hming tur dah la
+global.botname = "dengi" // I bot hming tur dah la
 global.ownerweb = "https://youtube.com/@HBMods_Channel"
 global.themeemoji = '🤖'
-global.creator = "916009259402@s.whatsapp.net" // @ symbol hmaa number hi i phone number in thlak rawh
+global.creator = "918798751883@s.whatsapp.net" // @ symbol hmaa number hi i phone number in thlak rawh
 
-global.ownerNumber = ["916009259402@s.whatsapp.net"]// @ symbol hmaa number hi i phone number in thlak rawh
+global.ownerNumber = ["918798751883@s.whatsapp.net"]// @ symbol hmaa number hi i phone number in thlak rawh
 global.prefa = ['','!','.','#','/']
 global.sessionName = 'session'
 
 // Sticker Pack Name
-global.packname = "Sticker By Dream guy yato🦄"
-global.author = "🦄dream guy yato"
+global.packname = "Sticker By 🦄dengi"
+global.author = "🦄dengi"
 global.hbmods = "https://herbert70.blogspot.com/"
 //_______________________________________________
 global.defaultpp = 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png?q=60'
